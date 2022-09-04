23446aa81d5b1060570a86fe8751b8c5a9ce9416
jane__ <janecching@gmail.com>
2022-09-03 22:23:34 -0400

    Added preservation methods feature

--- send_sms.py
+++ send_sms.py
@@ -10,21 +10,32 @@ auth_token = "c8f2c5002da0277fcfc72c4d0c3fc0e7"
 client = Client(account_sid, auth_token)
 
 lst = ["apple", "pear", "banana"] #to be linked to database
+
+dict = {"apple": "freeze apple for smoothies", 
+        "pear": "wrap pear tightly with plastic wrap and refrigerate",
+        "banana": "freeze bananas for smoothies."
+        }
+
+
+
 foodStr = ""
+hints = ""
 if len(lst) == 1:
     foodStr += lst[0]
 else:
     for i in range(len(lst)-1): 
         foodStr += f"{lst[i]}, "
+        hints += f"{dict[lst[i]]}, "
     foodStr += f"and {lst[-1]}"
+    hints += f"{dict[lst[-1]]}. "
+
+
+
 
 
 message = client.messages \
                 .create(
-                     body=f"Morning Jane! Just a small reminder\
-                         that your {foodStr} might go bad today so\
-                         remember to use or preserve it soon!\n\
-                         Love, your GreenFridge",
+                     body=f"\nMorning Jane! Just a small reminder that your {foodStr} might go bad today so remember to use or preserve it soon!\n\nHint: {hints}\n\nLove, your GreenFridge <3",
                      from_='+15737874445',
                      to='+14154167799'
                  )
